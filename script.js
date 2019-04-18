84c8a6d5fc6b99a9c53cbac7850c98e6e2e3cb92
Misha <zuldar1997@gmail.com>
2019-04-18 14:06:27 +0300

    Первое добавление

---
+++ script.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+
+console.log(4/0);
+console.log();
+
+let person = {
+    name:"John",
+    age: "25",
+    isMarried: true
+};
+
+console.log(person["name"]);
+
+let arr = ['plum.png','orange.jpg','apple.bmp']
+
+console.log(arr[2])
+
+//alert("Hello")
+
+//let answer = confirm("Hello ?");
+//console.log(answer)
+
+let answer = prompt("Есть ли вам 18?", "Да");
+console.log(answer);
